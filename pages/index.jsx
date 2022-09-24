eeed8fa4bd38780a755bc30b76fc1dca552db07a
ncyeh1 <ujmnej@gmail.com>
2022-09-24 20:46:08 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "l-FA13P5Sum0pJcnhxomXi2zisoQVUwK"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "l-FA13P5Sum0pJcnhxomXi2zisoQVUwK"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
